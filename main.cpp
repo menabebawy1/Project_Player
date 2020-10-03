d98c8e3c21560bf26a7349fec98750fab64e9399
menabebawy1 <mena.m.bebawy@gmail.com>
2020-10-02 23:30:14 -0400

    fixed bob test

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 int main(){
     std::cout << "Player 1: " << std::endl;
     std::cout << std::endl;
-    
+
     Player myplayer("Peter Parker", "Spurs", 0);
     std::cout << "Your player level is: " << myplayer.getLEVEL() << std::endl;
     std::cout << "Your player name is: " << myplayer.getNAME() << std::endl;
@@ -23,14 +23,16 @@ int main(){
     std::cout << "Your player level is: " << myplayer2.getLEVEL() << std::endl;
     std::cout << "Your player fitness level is: " << myplayer2.getFITNESS() << std::endl;
     std::cout << "Your player ppg is: " << myplayer2.getPPG() << std::endl;
-    myplayer2.run(10000);
+    myplayer2.run(50);
+    myplayer2.run(50);
     myplayer2.shoot(15000);
-    myplayer2.run(10000);
+    myplayer2.run(50);
     myplayer2.shoot(15000);
+    myplayer2.run(50);
     std::cout << "Your player level is: " << myplayer2.getLEVEL() << std::endl;
     std::cout << "Your player fitness level is: " << myplayer2.getFITNESS() << std::endl;
     std::cout << "Your player ppg is: " << myplayer2.getPPG() << std::endl;
-    myplayer.summary();
+    myplayer2.summary();
 
 
 
