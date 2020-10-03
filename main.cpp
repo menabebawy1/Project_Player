615a06e6cfeb0043e7e6f48802201cb1a83775be
menabebawy1 <mena.m.bebawy@gmail.com>
2020-10-02 23:16:01 -0400

    implemented functions

--- main.cpp
+++ main.cpp
@@ -2,5 +2,15 @@
 #include "basketball.hpp"
 
 int main(){
-    
+    Player myplayer("Peter Parker", "Spurs", 0);
+    std::cout << "Your player level is: " << myplayer.getLEVEL() << std::endl;
+    std::cout << "Your player name is: " << myplayer.getNAME() << std::endl;
+    std::cout << "Your player number is: " << myplayer.getNUM() << std::endl;
+    myplayer.run(1000);
+    myplayer.shoot(2000);
+    std::cout << "Your player level is: " << myplayer.getLEVEL() << std::endl;
+    myplayer.run(1000000);
+    myplayer.summary();
+
+
 }
