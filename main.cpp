81c1a9e0fa13327af8f3545ec190e77c38e7278a
menabebawy1 <mena.m.bebawy@gmail.com>
2020-10-02 23:27:40 -0400

    added bob player

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include "basketball.hpp"
 
 int main(){
+    std::cout << "Player 1: " << std::endl;
+    std::cout << std::endl;
+    
     Player myplayer("Peter Parker", "Spurs", 0);
     std::cout << "Your player level is: " << myplayer.getLEVEL() << std::endl;
     std::cout << "Your player name is: " << myplayer.getNAME() << std::endl;
@@ -9,8 +12,26 @@ int main(){
     myplayer.run(1000);
     myplayer.shoot(2000);
     std::cout << "Your player level is: " << myplayer.getLEVEL() << std::endl;
-    myplayer.run(1000000);
+    myplayer.run(10000);
     myplayer.summary();
+    std::cout << std::endl;
+
+    std::cout << "Player 2: " << std::endl;
+    std::cout << std::endl;
+
+    Player myplayer2("BOB", "Warriors", 100);
+    std::cout << "Your player level is: " << myplayer2.getLEVEL() << std::endl;
+    std::cout << "Your player fitness level is: " << myplayer2.getFITNESS() << std::endl;
+    std::cout << "Your player ppg is: " << myplayer2.getPPG() << std::endl;
+    myplayer2.run(10000);
+    myplayer2.shoot(15000);
+    myplayer2.run(10000);
+    myplayer2.shoot(15000);
+    std::cout << "Your player level is: " << myplayer2.getLEVEL() << std::endl;
+    std::cout << "Your player fitness level is: " << myplayer2.getFITNESS() << std::endl;
+    std::cout << "Your player ppg is: " << myplayer2.getPPG() << std::endl;
+    myplayer.summary();
+
 
 
 }
